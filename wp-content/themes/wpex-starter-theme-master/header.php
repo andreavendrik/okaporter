039f2287b0d75533440e6eb380e96d291119b247
andreavendrik <andrea.vendrik@gmail.com>
2017-02-22 16:11:12 +0100

    Removed scripts from header.php

--- wp-content/themes/wpex-starter-theme-master/header.php
+++ wp-content/themes/wpex-starter-theme-master/header.php
@@ -7,7 +7,6 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-	<script src="<?php echo get_bloginfo('siteurl');?>/wp-content/themes/wpex-starter-theme-master/js/slideout/dist/slideout.min.js"></script>
 </head>
 
 
@@ -70,67 +69,3 @@
 			</section>
 
 		</nav>
-
-<script>
-
-//adds styling to main menu on scroll
-
-	jQuery(window).scroll(function() {     
-		var scroll = jQuery(window).scrollTop();
-		if (scroll > 60) {
-			jQuery(".menu-wrapper").addClass("active");
-		}
-		else {
-			jQuery(".menu-wrapper").removeClass("active");
-		}
-});
-
-
-//adds styling and pulls out filter menu when filter button is clicked on mobile 
-
-	jQuery(document).ready(function(){
-		jQuery('#filter-button-mobile').live('click', function(event) {
-			jQuery('.filter-labels').slideToggle(200,'swing');
-			jQuery('#filter-button-mobile').toggleClass('filter-button-mobile-active'); 
-			jQuery('#menu-filter-heading').toggleClass('menu-filter-heading-active'); 
-			jQuery('.main-menu').toggleClass('filter-button-active');  	
-			jQuery('#menu-logo').toggleClass('hide-menu-logo');  								
-		});    
-});
-
-//adds styling to active filter button on desktop 
-	jQuery(document).ready(function(){
-		jQuery('.filter-labels label').live('click', function(event) {
-				jQuery('.filter-labels label').addClass('filter-labels-active');        			
-		  });
-		});  
-
-//slide out menu
-
-	jQuery(document).ready(function(){
-		jQuery('#menu-toggle-button').live('click', function(event) {
-			jQuery('#mobile-menu-slideout-panel').toggleClass('mobile-menu-slideout-panel-active');   
-			jQuery('.website-wrapper').toggleClass('website-wrapper-active');          
-			jQuery('.menu-wrapper').toggleClass('menu-wrapper-active');           			 
-			jQuery('#menu-toggle-button').toggleClass('menu-toggle-button-active');    
-		  });
-		});  
-
-</script>
-
-<script>
-
-	var scrollTimer = null;
-
-	jQuery(document).ready(function(){
-
-		jQuery(window).on('scroll', function() {
-		    jQuery('.map').css("pointer-events", "none");
-		    window.clearTimeout(scrollTimer);
-			scrollTimer = window.setTimeout(function() {
-			    jQuery('.map').css("pointer-events", "auto");
-			}, 500);
-		})
-	
-	});  
-</script>
