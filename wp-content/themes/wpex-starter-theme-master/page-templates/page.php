07fd0085b514375885aa77ea6031995382a50109
andreavendrik <andrea.vendrik@gmail.com>
2017-02-22 16:11:51 +0100

    Added newsletter form

--- wp-content/themes/wpex-starter-theme-master/page-templates/page.php
+++ wp-content/themes/wpex-starter-theme-master/page-templates/page.php
@@ -21,6 +21,30 @@ $content = $post->post_content;?>
 		<?php the_field ('about_go_frank') ?>
 	</div>
 
+	<div class="page-content" id="mailchimp-form-wrapper">
+		<div class="mailchimp-form">
+			<h3>To stay up to date on new brands and all that, subscribe to our newsletter!</h3>
+			<form action="//gofrank.us15.list-manage.com/subscribe/post?u=b4cb67be017f0bcbe31903598&amp;id=92def1d047" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			    <div class="mc-newsletter-application">
+					<div class="mc-field-group">
+						<label for="mce-EMAIL">Email Address:</label>
+						<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+					</div>
+					<div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b4cb67be017f0bcbe31903598_92def1d047" tabindex="-1" value=""></div>
+				    <div class="clear">
+				    	<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+				    </div>
+			    </div>
+			</form>
+				<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+			<!--End mc_embed_signup-->
+		</div>
+	</div>
+
 	<div class="page-content">
 	<div id="brand-application-section">
 		<h2>Your brand here?</h2>
