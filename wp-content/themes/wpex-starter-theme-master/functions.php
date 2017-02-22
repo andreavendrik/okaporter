8446bdf33ee89ed21c3f36c944c501da49ab068f
andreavendrik <andrea.vendrik@gmail.com>
2017-02-22 16:10:09 +0100

    Added randomised posts function

--- wp-content/themes/wpex-starter-theme-master/functions.php
+++ wp-content/themes/wpex-starter-theme-master/functions.php
@@ -176,3 +176,34 @@ class WPEX_Starter_Theme_Setup {
 
 // Start the class and set as variable for child-theming
 $wpex_starter_theme_setup = new WPEX_Starter_Theme_Setup;
+?>
+
+<?php
+session_start();
+add_filter( 'posts_orderby', 'randomise_with_pagination' );
+function randomise_with_pagination( $orderby ) {
+	if( is_front_page() ) {
+	  	// Reset seed on load of initial archive page
+		if( ! get_query_var( 'paged' ) || get_query_var( 'paged' ) == 0 || get_query_var( 'paged' ) == 1 ) {
+			if( isset( $_SESSION['seed'] ) ) {
+				unset( $_SESSION['seed'] );
+			}
+		}
+	
+		// Get seed from session variable if it exists
+		$seed = false;
+		if( isset( $_SESSION['seed'] ) ) {
+			$seed = $_SESSION['seed'];
+		}
+	
+	    	// Set new seed if none exists
+	    	if ( ! $seed ) {
+	      		$seed = rand();
+	      		$_SESSION['seed'] = $seed;
+	    	}
+	
+	    	// Update ORDER BY clause to use seed
+	    	$orderby = 'RAND(' . $seed . ')';
+	}
+	return $orderby;
+}
